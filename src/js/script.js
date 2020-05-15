f0b1aaad8621dfc2c74a3ec00ff89d071c0eb23c
Кирилл <1chunaev@gmail.com>
2020-05-15 17:20:14 +0300

    Загрузка проекта SportNote, слайдер с таблицей не работает

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('.main-slider').slick({
+    arrows: true,
+    slidesToShow: 3,
+    vertical: true,
+    infinite: false,
+    asNavFor: ".info-slider",
+    initialSlide: 0
+  });
+
+  $('.info-slider').slick({
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    vertical: true,
+    infinite: false,
+    asNavFor: ".main-slider",
+    initialSlide: 0
+  });
+
+  // $('.main-slider__item').click(function(){
+  //   $('.main-slider__item').toggleClass('active');
+  // });
+});
